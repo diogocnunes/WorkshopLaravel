d781eb4ddef85b971b89d1b98d4966c303abb9bf
Diogo Nunes <diogo.nunes@pdmfc.com>
2019-06-28 10:32:56 +0100

    Class 1 and 2

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function welcome() {
+        return view('welcome')->withMovies([
+            'Avengers: Endgame',
+            'Star Wars',
+            'Lord of the Rings'
+        ]);
+    }
+}
