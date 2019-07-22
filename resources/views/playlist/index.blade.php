db350491b2f1617aa4e4dfbf47ddaf8c8b89c609
João Santos <joao.santos@pdmfc.com>
2019-07-22 20:06:06 +0100

    wip

--- resources/views/playlist/index.blade.php
+++ resources/views/playlist/index.blade.php
@@ -3,5 +3,17 @@
 @section ('title', 'Cineflix | Playlist')
 
 @section ('content')
+<div class="mt-20">
+    <h1 class="uppercase font-bold tracking-wider mx-2">Latest Movies</h1>
 
+    <div class="flex flex-wrap">
+        @foreach ($movies as $movie)
+            <div class="my-4 mx-2 rounded">
+                <a href="{{ $movie->path() }}">
+                    <img src="{{ $movie->thumbnail }}" alt="" style="width: 260px; height: 150px; object-fit: cover;" class="rounded-sm shadow">
+                </a>
+            </div>
+        @endforeach
+    </div>
+</div>
 @endsection
