ea2f5793019024eff2723d457d808320e356996d
Diogo Nunes <diogo.nunes@pdmfc.com>
2019-07-23 15:58:50 +0100

    Aula 15 - Teste Resources

---
+++ app/Http/Resources/MovieListResource.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class MovieListResource extends ResourceCollection
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        $this->collection->each(function ($row) use (&$results) {
+            $newRow = [
+                'id' => $row->id,
+                'title' => $row->title,
+                'description' => $row->description,
+                'year' => $row->year,
+                'cover' => $row->cover,
+                'genre' => $row->genre->name,
+            ];
+            $results[] = $newRow;
+        });
+
+        return [
+            'data' => $results,
+            'meta' => [
+                'total' => count($this->collection)
+            ]
+        ];
+    }
+}
